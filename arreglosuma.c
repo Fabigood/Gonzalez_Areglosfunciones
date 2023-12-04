254139c0c7eaa71c35a6c9fe32fca1869db0bf34
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-12-03 23:52:31 -0500

    final

--- arreglosuma.c
+++ arreglosuma.c
@@ -5,13 +5,13 @@
 void crearMatrix(int n, int m, int matrix[n][m]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) { //esta funcion void es utilizada para crear la matrix con num randoms
-            matrix[i][j] = rand() % 100 + 1;
+            matrix[i][j] = rand() % 100 + 1;//generador de valor 1 a 100
         }
     }
 }
 void printMatrix(int n, int m, int matrix[n][m]) {
     for (int i = 0; i < n; i++) {
-        printf("\n");
+        printf("\n"); //void crear solo para imprimir 
         for (int j = 0; j < m; j++) {
             printf("%d\t", matrix[i][j]);
                 }
@@ -20,7 +20,7 @@ void printMatrix(int n, int m, int matrix[n][m]) {
             }
 int main()
 {
-    srand(time(NULL));
+    srand(time(NULL));//comadno de num random explicado en clase
     int n, m;
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &n); // se pide al usuario el tamana de la matrix
@@ -32,20 +32,20 @@ int main()
     int Matrixre[n][m];
 
     printf("primera matrix");
-    crearMatrix(n, m, Matrix);
+    crearMatrix(n, m, Matrix); //se llama a las funciones
     printMatrix(n, m, Matrix);
 
     printf("segunda matriz\n");
     crearMatrix(n, m, Matrix2);
-    printMatrix(n, m, Matrix2);
+    printMatrix(n, m, Matrix2); //llamar funcione
 
      printf("\nResultado de la suma de las matrices:\n");
     for (int i = 0; i < n; i++)
     {
         printf("\n");
         for (int j = 0; j < m; j++)
-        {//se crea el for apra sumar las matrices dadas
-            Matrixre[i][j] = Matrix[i][j] + Matrix2[i][j];
+        {//se crea el for paraa sumar las matrices dadas
+            Matrixre[i][j] = Matrix[i][j] + Matrix2[i][j];//suma de matrices
             printf("%d\t", Matrixre[i][j]);
 
         }
